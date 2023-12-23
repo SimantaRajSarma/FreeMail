0b421abe56175468f3b6ea2622c8e57954aa829b
SimantaRajSarma <139645750+SimantaRajSarma@users.noreply.github.com>
2023-12-24 01:27:24 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -14,18 +14,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mail = new PHPMailer(true);
 
 try {
-	$mail->SMTPDebug = 2;									 
+	// $mail->SMTPDebug = 2; 									 
 	$mail->isSMTP();										 
-	$mail->Host	 = 'smtp.gmail.com';					// Use Gmail SMTP server
+	$mail->Host = 'smtp.gmail.com';					// Gmail SMTP server
 	$mail->SMTPAuth = true;							 
-	$mail->Username = 'example@gmail.com';			// Your Gmail email address
+	$mail->Username = 'example@gmail.com';			// Your email address
 	$mail->Password = 'your app password';					// Your App password
 	$mail->SMTPSecure = 'tls';							 
-	$mail->Port	 = 587; 
+	$mail->Port = 587; 
 
 	// Modify the email content to include form data
-	$mail->Subject = 'Contact Form Submission';
-	$mail->Body = "Name: $name<br>Email: $email<br>Message: $message";
+	$mail->Subject = 'Hi ' . $name;
+	$mail->Body = "<h4> Message: $message</h4>";
 	// Other mail settings
 
 	// Add recipient email addresses (you can customize this based on your needs)
@@ -35,9 +35,18 @@ try {
 
 	// Send the email
 	$mail->send();
-	echo "Mail has been sent successfully!";
+	// Output HTML with JavaScript alert
+	echo '<script>';
+	echo 'alert("Mail has been sent successfully!");';
+	echo 'window.location.href = "index.html";'; // Redirect after showing the alert
+	echo '</script>';
+	
 } catch (Exception $e) {
-	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+	// echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+	echo '<script>';
+	echo 'alert("Mail could not be send. Try Again!");';
+	echo 'window.location.href = "index.html";'; // Redirect after showing the alert
+	echo '</script>';
 }
 } else {
     // Redirect or handle non-POST requests as needed
